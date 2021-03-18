eb81aff6bb3a780038ac50dd07d9c1b3afc0ac11
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-18 16:32:32 +0200

    Updates

---
+++ precache-manifest.0d54f2e476db637d763c334eab9bed76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5c5664c0518731671c73bdc62f9094",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e6cf1c1c90591093541",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6cf1c1c90591093541",
+    "url": "./static/js/main.ff88e447.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
